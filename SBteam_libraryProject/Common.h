6cfc179a018a8b52644b3ae156dcac7358e19cba
sekal bashir <assassinbachir@gmail.com>
2026-04-27 22:25:23 +0100

    Changing arraylists with liked lists.

--- SBteam_libraryProject/Common.h
+++ SBteam_libraryProject/Common.h
@@ -9,31 +9,24 @@
 #define MAX_STRING_LEN  256
 
 /* Cursor-based singly linked list */
-struct ArrayList {
-    int data[MAX_LIST_SIZE];
-    int next[MAX_LIST_SIZE];   // next[i] = index of successor, -1 = end
-    int head;                   // index of first element, -1 = empty
-    int size;
+struct Node {
+    int data;
+    Node* next;
 };/* Cursor-based doubly linked list */
-struct DLL {
-    int data[MAX_LIST_SIZE];
-    int next[MAX_LIST_SIZE];
-    int prev[MAX_LIST_SIZE];
-    int head;
-    int tail;
-    int size;
+struct dNode {
+    int data;
+    dNode* prev;
+    dNode* next;    
 };
 
 /* Array-based Stack */
 struct Stack {
-    int data[MAX_STACK_SIZE];
-    int top;    // index of topmost element, -1 = empty
+    Node* top;   // top element
 };
 
 /* Circular Array Queue */
 struct Queue {
-    int data[MAX_QUEUE_SIZE];
-    int front;  // index of front element
-    int rear;   // index of last element
-    int count;  // number of elements
+    Node* front;  // first element
+    Node* rear;   // last element
+    int count;
 };
